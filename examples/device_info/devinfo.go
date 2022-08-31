4dc1f19c97ad72be14d4d21e93a8e61bfcfd2814
Java2019 <java_2019@mail.ru>
2022-08-31 16:28:50 +0300

    Update devinfo.go

--- examples/device_info/devinfo.go
+++ examples/device_info/devinfo.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"strings"
 
-	device2 "github.com/vladimirvivien/go4vl/device"
-	"github.com/vladimirvivien/go4vl/v4l2"
+	device2 "github.com/Java2019/go4vlv1/device"
+	"github.com/Java2019/go4vlv1/v4l2"
 )
 
 var template = "\t%-24s : %s\n"
