6bc3c27384566b2fc69a514a3d79fd1e71b73dc3
Java2019 <java_2019@mail.ru>
2022-08-31 14:17:48 +0300

    Update capability.go

--- v4l2/capability.go
+++ v4l2/capability.go
@@ -1,9 +1,9 @@
 package v4l2
 
-/*
-#cgo linux CFLAGS: -I ${SRCDIR}/../include/
-#include <linux/videodev2.h>
-*/
+//*
+//#cgo linux CFLAGS: -I ${SRCDIR}/../include/
+//#include <linux/videodev2.h>
+//*/
 import "C"
 
 import (
